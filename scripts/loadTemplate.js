290530102c4cf1c219966edad91ef473c63bc420
dionisio-rodrigues <d.rodriguesmaianeto@gmail.com>
2022-07-03 00:26:10 -0300

    adicionando tabs com bootstrap

---
+++ scripts/loadTemplate.js
@@ -0,0 +1,139 @@
+let template = document.getElementsByTagName("body");
+template[0].innerHTML = changeTab(template[0].getAttribute("content"));
+
+function changeTab(valueAttribute){
+    var htmlCode;
+    if(valueAttribute == "home"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link active" aria-current="page" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }else if(valueAttribute == "add"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a <a class="nav-link active" aria-current="page" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }else if(valueAttribute == "favorites"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link active" aria-current="page" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }else if(valueAttribute == "mylinks"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link active" aria-current="page" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }else if(valueAttribute == "settings"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link active" aria-current="page" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }else if(valueAttribute == "donate"){
+        htmlCode = '<ul class="nav nav-tabs">'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../index.html">Home</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/add.html">Add</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/favorites.html">Favorites</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/mylinks.html">My links</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+            '<a class="nav-link" href="../html/settings.html">Settings</a>'+
+        '</li>'+
+        '<li class="nav-item">'+
+        '<a class="nav-link active" aria-current="page" href="../html/donate.html">Coffee</a>'+
+        '</li>'+
+        '</ul>';
+    }
+
+    console.log(valueAttribute)
+    console.log(htmlCode)
+
+    return htmlCode;
+}
+
